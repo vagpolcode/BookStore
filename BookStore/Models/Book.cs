7ac023f19e0612c0629b474b8d7a2d17a3557d77
vagpol5 <vagpol5@VPC>
2023-06-18 18:21:45 +0300

    Add project files.

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string info { get; set; }
+
+        public int price { get; set; }
+
+        public string author { get; set; }
+
+
+    }
+}
